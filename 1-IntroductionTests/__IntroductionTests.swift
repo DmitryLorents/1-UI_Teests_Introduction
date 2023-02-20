8fdab824f8c06e885ea26817867dba3df4d50f73
DmitryLorents <zmey.task@gmail.com>
2023-02-20 21:20:11 +0600

    - rename project - import UnitTestIntroduction - add fieldValidator

--- 1-IntroductionTests/__IntroductionTests.swift
+++ 1-IntroductionTests/__IntroductionTests.swift
@@ -6,14 +6,18 @@
 //
 
 import XCTest
+@testable import UnitTestIntroduction
 
 class __IntroductionTests: XCTestCase {
+    
+    var fieldValidator: FieldValidator!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
     override func tearDownWithError() throws {
+        
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
