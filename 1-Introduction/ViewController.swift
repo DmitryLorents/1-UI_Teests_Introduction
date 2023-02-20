7f15ef264a4789f76705f74d0a7f3fe516c3301d
DmitryLorents <zmey.task@gmail.com>
2023-02-20 19:20:16 +0600

    - ViewController 	delegate functionality added

--- 1-Introduction/ViewController.swift
+++ 1-Introduction/ViewController.swift
@@ -7,15 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var loginTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginTF.delegate = self
+        passwordTF.delegate = self
+        
+        loginTF.returnKeyType = .continue
+        passwordTF.returnKeyType = .done
         
     }
-
+//MARK: - UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch  textField {
+        case loginTF:
+            passwordTF.becomeFirstResponder()
+        case passwordTF:
+            passwordTF.resignFirstResponder()
+            print("Done pressed")
+        default: break
+        }
+        return true
+    }
 
 }
 
